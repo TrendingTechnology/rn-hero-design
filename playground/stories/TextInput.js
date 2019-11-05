1acca9130f40ffb260a672c790bb01e718fb30e2
Toan Nguyen <toan2406@gmail.com>
2019-11-05 11:46:45 +0700

    Update TextInput component

--- playground/stories/TextInput.js
+++ playground/stories/TextInput.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput } from 'rn-hero-design';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'rn-hero-design';
 
 const TextInputScreen = () => (
-  <View style={styles.container}>
-    <TextInput placeholder="email" />
-    <TextInput placeholder="email" value="toan.nguyen@employmenthero.com" />
-  </View>
+  <SafeAreaView style={styles.safeArea}>
+    <View style={styles.container}>
+      <View style={styles.inputGroup}>
+        <TextInput
+          label="Email"
+          value="toan.nguyen@employmenthero.com"
+          rightIcon="email-outline"
+        />
+
+        <TextInput
+          label="Password"
+          value="123456"
+          rightIcon="eye-outline"
+          secureTextEntry
+        />
+
+        <TextInput
+          label="Confirm password"
+          value="12345"
+          rightIcon="eye-outline"
+          secureTextEntry
+          error="Password not matched"
+          wrapperStyle={{ marginBottom: 32 }}
+        />
+
+        <TextInput label="Region" value="Vietnam" disabled />
+      </View>
+
+      <Button text="Register" onPress={() => {}} />
+    </View>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
-    padding: 16,
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  inputGroup: {
+    flex: 1,
   },
 });
 
