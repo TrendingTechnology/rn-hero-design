4091659823165e96b1a58982190dbb214a0377e8
Toan Nguyen <toan2406@gmail.com>
2019-11-18 17:53:30 +0700

    Update BottomButton and add playground for it

--- playground/stories/BottomButton.js
+++ playground/stories/BottomButton.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import { View, Alert, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  Switch,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+} from 'react-native';
 import { BottomButton } from 'rn-hero-design';
+import legacyTheme from 'rn-hero-design/src/themes/legacy';
 
-const callback = () => Alert.alert('Button clicked!');
+const noop = () => {};
 
-const BottomButtonScreen = () => (
-  <View style={styles.container}>
-    <View style={styles.pusher} />
-    <BottomButton text="Bottom Button" onPress={callback} />
-    <BottomButton text="Bottom Button" onPress={callback} />
-  </View>
-);
+const BottomButtonScreen = () => {
+  const [useLegacy, setUseLegacy] = useState(false);
+  const [showKeyboard, setShowKeyboard] = useState(false);
+  const input = useRef(null);
+
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={88}
+      enabled
+      style={styles.keyboardAvoidingView}
+    >
+      <View style={styles.container}>
+        <View style={styles.configRow}>
+          <Text>Use legacy theme</Text>
+          <Switch
+            value={useLegacy}
+            onValueChange={value => setUseLegacy(value)}
+          />
+        </View>
+
+        <View style={styles.configRow}>
+          <Text>Show keyboard</Text>
+          <Switch
+            value={showKeyboard}
+            onValueChange={value => {
+              value ? input.current.focus() : input.current.blur();
+              setShowKeyboard(value);
+            }}
+          />
+        </View>
+
+        <TextInput ref={input} style={styles.input} />
+      </View>
+
+      <BottomButton
+        text="Bottom Button"
+        onPress={noop}
+        theme={useLegacy ? legacyTheme : undefined}
+      />
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  keyboardAvoidingView: {
     flex: 1,
   },
-  pusher: {
+  container: {
     flex: 1,
+    paddingVertical: 8,
+  },
+  configRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  input: {
+    width: 1,
+    height: 1,
+    opacity: 0,
   },
 });
 
