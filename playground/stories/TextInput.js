cd389f249298e6f8c05bb19ec7685aefd6e57835
Toan Nguyen <toan2406@gmail.com>
2019-11-20 00:47:04 +0700

    Update TextInput story

--- playground/stories/TextInput.js
+++ playground/stories/TextInput.js
@@ -1,64 +1,57 @@
 import React from 'react';
-import { View, SafeAreaView, StyleSheet } from 'react-native';
-import { TextInput, Button, Text } from 'rn-hero-design';
+import { ScrollView, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { TextInput, BottomButton, Text } from 'rn-hero-design';
 
 const noop = () => {};
 
 const TextInputScreen = () => (
-  <SafeAreaView style={styles.safeArea}>
-    <View style={styles.container}>
-      <View style={styles.inputGroup}>
-        <TextInput
-          label="Email"
-          value="toan.nguyen@employmenthero.com"
-          rightIcon="email-outline"
-          onChangeText={noop}
-        />
+  <KeyboardAvoidingView
+    behavior="padding"
+    keyboardVerticalOffset={88}
+    enabled
+    style={styles.keyboardAvoidingView}
+  >
+    <ScrollView contentContainerStyle={styles.container}>
+      <TextInput
+        label="Email"
+        value="toan.nguyen@employmenthero.com"
+        rightIcon="email-outline"
+        onChangeText={noop}
+      />
 
-        <TextInput
-          label="Password"
-          value="123456"
-          rightIcon="eye-outline"
-          onChangeText={noop}
-          secureTextEntry
-        />
+      <TextInput
+        label="Password"
+        value="123456"
+        rightIcon="eye-outline"
+        onChangeText={noop}
+        secureTextEntry
+      />
 
-        <TextInput
-          label="Confirm password"
-          value="12345"
-          rightIcon="eye-outline"
-          onChangeText={noop}
-          secureTextEntry
-          error="Password not matched"
-          wrapperStyle={{ marginBottom: 32 }}
-        />
+      <TextInput
+        label="Confirm password"
+        value="12345"
+        rightIcon="eye-outline"
+        onChangeText={noop}
+        secureTextEntry
+        error="Password not matched"
+        wrapperStyle={{ marginBottom: 32 }}
+      />
 
-        <TextInput
-          label="Region"
-          value="Vietnam"
-          disabled
-          onChangeText={noop}
-        />
+      <TextInput label="Region" value="Vietnam" disabled onChangeText={noop} />
 
-        <Text>* By signing up, you agree to our terms and conditions</Text>
-      </View>
+      <Text>* By signing up, you agree to our terms and conditions</Text>
+    </ScrollView>
 
-      <Button text="Register" onPress={() => {}} />
-    </View>
-  </SafeAreaView>
+    <BottomButton text="Register" onPress={() => {}} />
+  </KeyboardAvoidingView>
 );
 
 const styles = StyleSheet.create({
-  safeArea: {
+  keyboardAvoidingView: {
     flex: 1,
   },
   container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    paddingTop: 16,
-  },
-  inputGroup: {
-    flex: 1,
+    padding: 16,
   },
 });
 
