1254c47eb959a0d9ddb7f8263017441c89e5212d
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-01-16 16:00:22 +0700

    Handle select dialog on Android

--- lib/src/themes/hero/Hero_Select.re
+++ lib/src/themes/hero/Hero_Select.re
@@ -11,6 +11,12 @@ let _ACTION_PADDING_HORIZONTAL = _MEDIUM_SIZE;
 let _ACTION_HEIGHT =
   _ACTION_TEXT_LINE_HEIGHT +. _ACTION_PADDING_VERTICAL *. 2.0;
 
+let _OPTION_PADDING_VERTICAL = _MEDIUM_SIZE;
+let _OPTION_PADDING_HORIZONTAL = _MEDIUM_SIZE;
+let _OPTION_TEXT_FONT_SIZE = _HEADER_4;
+let _OPTION_TEXT_COLOR = _TEXT_COLOR;
+let _SELECTED_OPTION_TEXT_COLOR = _PRIMARY_COLOR;
+
 let styles: Js.t('a) = {
   "wrapper": style(~backgroundColor=_WRAPPER_BACKGROUND_COLOR, ()),
   "actions":
@@ -31,6 +37,34 @@ let styles: Js.t('a) = {
   /* Select uses system font */
   "actionText":
     style(~fontSize=_ACTION_TEXT_FONT_SIZE, ~color=_ACTION_TEXT_COLOR, ()),
+
+  "overlay":
+    style(
+      ~flex=1.0,
+      ~alignItems=`center,
+      ~justifyContent=`center,
+      ~backgroundColor="rgba(0,0,0,0.5)",
+      (),
+    ),
+  "dialog":
+    style(
+      ~width=80.0->pct,
+      ~maxHeight=70.0->pct,
+      ~backgroundColor="white",
+      ~borderRadius=2.0,
+      ~elevation=10.0,
+      (),
+    ),
+  "optionWrapper":
+    style(
+      ~paddingVertical=_OPTION_PADDING_VERTICAL->dp,
+      ~paddingHorizontal=_OPTION_PADDING_HORIZONTAL->dp,
+      (),
+    ),
+  "optionText":
+    style(~fontSize=_OPTION_TEXT_FONT_SIZE, ~color=_OPTION_TEXT_COLOR, ()),
+  "selectedOptionText":
+    style(~fontWeight=`_400, ~color=_SELECTED_OPTION_TEXT_COLOR, ()),
 };
 
 let default: Js.t('a) = ReactNative.StyleSheet.create(styles);
--- playground/stories/Select.js
+++ playground/stories/Select.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import { Container, Select } from 'rn-hero-design';
+import React, { useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Container, Select, TextInput } from 'rn-hero-design';
 
-const options = [
-  { label: 'Vietnam', value: 'vi' },
+const noop = () => {};
+
+const languages = [
+  { label: 'Tiếng Việt', value: 'vi' },
   { label: 'English', value: 'en' },
+  { label: 'Deutsch', value: 'de' },
+  { label: 'Español', value: 'es' },
+  { label: 'Français', value: 'fr' },
+  { label: '日本語', value: 'ja' },
+  { label: '한국어', value: 'ko' },
+  { label: 'Bahasa Melayu', value: 'ms' },
+  { label: 'Русский', value: 'ru' },
+  { label: 'ไทย', value: 'th' },
+  { label: '中文', value: 'zh' },
 ];
 
-const SelectScreen = () => (
-  <>
-    <Container fluid></Container>
+const genders = [
+  { label: 'Male', value: '1' },
+  { label: 'Female', value: '0' },
+];
 
-    <Select show value="en" options={options} />
-  </>
-);
+const getLabelOfValue = (options, value) => {
+  const selectedOption = options.find(option => option.value === value);
+  return selectedOption ? selectedOption.label : '';
+};
+
+const SelectScreen = () => {
+  const [language, setLanguage] = useState('');
+  const [gender, setGender] = useState('');
+  const [changingField, setChangingField] = useState(null);
+
+  const fieldMap = {
+    gender: {
+      options: genders,
+      value: gender,
+      onChange: setGender,
+    },
+    language: {
+      options: languages,
+      value: language,
+      onChange: setLanguage,
+    },
+    null: {
+      options: [],
+      value: '',
+      onChange: noop,
+    },
+  };
+
+  return (
+    <>
+      <Container fluid>
+        <TouchableOpacity onPress={() => setChangingField('gender')}>
+          <View pointerEvents="none">
+            <TextInput
+              label="Gender"
+              value={getLabelOfValue(genders, gender)}
+              onChangeText={noop}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setChangingField('language')}>
+          <View pointerEvents="none">
+            <TextInput
+              label="Language"
+              value={getLabelOfValue(languages, language)}
+              onChangeText={noop}
+            />
+          </View>
+        </TouchableOpacity>
+      </Container>
+
+      <Select
+        show={!!changingField}
+        options={fieldMap[changingField].options}
+        value={fieldMap[changingField].value}
+        onChange={value => fieldMap[changingField].onChange(value)}
+        onDismiss={() => setChangingField(null)}
+      />
+    </>
+  );
+};
 
 export default SelectScreen;
