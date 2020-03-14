ed6bf3af0764526906da72926fba954f2da14f01
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-03-14 18:23:22 +0700

    Add color list component

---
+++ documentation/components/ColorList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Text } from 'rn-hero-design';
+
+const IconList = ({ colors = [] }) => (
+  <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+    {colors.map(([name, code]) => (
+      <div
+        key={name}
+        title={name}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          width: 80,
+          height: 80,
+          margin: 12,
+        }}
+      >
+        <div
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 4,
+            background: code,
+          }}
+        />
+        <Text
+          size="h5"
+          style={{
+            width: 80,
+            marginTop: 16,
+            textAlign: 'center',
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+          }}
+        >
+          {name}
+        </Text>
+      </div>
+    ))}
+  </div>
+);
+
+export default IconList;
