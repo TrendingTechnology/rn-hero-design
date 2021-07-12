e8fd278b41e002fa346283485152fe3a7733ecf1
Kien Tran <kien.tran@employmenthero.com>
2021-07-12 14:02:02 +0700

    add gen type files

---
+++ lib/src/themes/hero/Hero_Badge.gen.tsx
@@ -0,0 +1,23 @@
+/* TypeScript file generated from Hero_Badge.re by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const Hero_BadgeBS = require('./Hero_Badge.bs');
+
+import {Style_t as ReactNative_Style_t} from '../../../src/shims/ReactNative.shim';
+
+export const $$default: {
+  readonly content: ReactNative_Style_t; 
+  readonly errorContent: ReactNative_Style_t; 
+  readonly errorWrapper: ReactNative_Style_t; 
+  readonly infoContent: ReactNative_Style_t; 
+  readonly infoWrapper: ReactNative_Style_t; 
+  readonly successContent: ReactNative_Style_t; 
+  readonly successWrapper: ReactNative_Style_t; 
+  readonly warningContent: ReactNative_Style_t; 
+  readonly warningWrapper: ReactNative_Style_t; 
+  readonly wrapper: ReactNative_Style_t
+} = Hero_BadgeBS.default;
+
+export default $$default;
---
+++ lib/src/themes/hero/Hero_ButtonGroup.gen.tsx
@@ -0,0 +1,21 @@
+/* TypeScript file generated from Hero_ButtonGroup.re by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const Hero_ButtonGroupBS = require('./Hero_ButtonGroup.bs');
+
+import {Style_t as ReactNative_Style_t} from '../../../src/shims/ReactNative.shim';
+
+export const $$default: {
+  readonly activeButton: ReactNative_Style_t; 
+  readonly activeText: ReactNative_Style_t; 
+  readonly button: ReactNative_Style_t; 
+  readonly group: ReactNative_Style_t; 
+  readonly inactiveButton: ReactNative_Style_t; 
+  readonly inactiveText: ReactNative_Style_t; 
+  readonly text: ReactNative_Style_t; 
+  readonly wrapper: ReactNative_Style_t
+} = Hero_ButtonGroupBS.default;
+
+export default $$default;
